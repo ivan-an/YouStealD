43bc74247894a3a21250f3185aeb707c4b8d314a
Praecipi <mails99995@gmail.com>
2026-03-13 19:59:40 +0300

    Add appconstants.h with application constants

---
+++ src/appconstants.h
@@ -0,0 +1,7 @@
+#ifndef APPCONSTANTS_H
+#define APPCONSTANTS_H
+
+constexpr const char* ORG_NAME = "YouTubeDownloader";
+constexpr const char* APP_NAME = "YouTubeDownloader";
+
+#endif // APPCONSTANTS_H
